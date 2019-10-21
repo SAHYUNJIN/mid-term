7a4c6de91bc7836bfd984542891cbbbca61466dd
SAHYUNJIN <tkguswls1106@gmail.com>
2019-10-21 10:53:28 +0900

    4

---
+++ 201921003-midterm-4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+	int i, num;
+	int j=1;
+	printf("Enter the number: ");
+	scanf("%d", &num);
+
+	for(i=1; i<=num; i++)
+		j=j*i;
+
+	printf("The factorial of %d is %d\n", num, j);
+}
+
+ /*
+  r
+  b 9
+  b 11
+  r
+  watch j
+  c
+  c
+  */
+
